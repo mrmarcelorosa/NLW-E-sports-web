0baa7803792cacc640bb5864dba2118e81e1b4b9
Marcelo Rosa <vyctor.rosa14@gmail.com>
2022-10-10 22:06:15 -0300

    building the web project landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './styles/main.css';
 import logoImg from './assets/Logo.svg'
+import {MagnifyingGlassPlus} from 'phosphor-react'
 
 function App() {
   return (
@@ -58,9 +59,22 @@ function App() {
 
       </div>
 
-      <div className='bg-[#2A2634] px-8 py-6 mt-8 self-stretch rounded-lg'>
+      <div className='pt-1 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 self-stretch flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400'>Publique um anúncio para encontrar novos players!</span>
+          </div>
 
-        
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio
+          </button>
+
+        </div>
+
+
+
 
       </div>
 
